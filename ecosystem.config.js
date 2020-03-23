d4b57d604c449aafda8c9ef90ecadc0d4c4a17e2
weikeduopp <1647090769@qq.com>
2020-03-23 15:02:51 +0800

    change repo: ssh

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,14 +15,15 @@ module.exports = {
     // "production" is the environment name
     production: {
       user: "manager",
+      // 可以多台服务器
       host: ["47.104.88.94"],
       // 默认22
       // port: "",
       ref: "origin/master",
-      repo: "https://github.com/weikeduoppp/express-blog.git",
-      path: "/www/blog/production", // 部署服务器的目录 -> 根目录
+      repo: "git@github.com:weikeduoppp/express-blog.git",
+      path: "/www/blog/production", // 部署到服务器的目录 -> 根目录
       ssh_options: ["StrictHostKeyChecking=no", "PasswordAuthentication=no"],
-      // 发布脚本
+      // 发布脚本  (部署到服务后, 服务器会执行的script)
       "post-deploy":
         "npm install; pm2 startOrRestart ecosystem.config.js --env production",
       // Environment variables that must be injected in all applications on this env
